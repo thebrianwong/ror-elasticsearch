07a56e6ea304cacdbe62a1ccb2b66081a344cb39
Brian Wong <brianw7218@gmail.com>
2024-04-24 14:58:27 -0700

    Add form to search for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
 class ProductsController < ApplicationController
   def index
-    query = Product.search("wine")
-    response = query.response["hits"]["hits"]
-    @products = response.map { |doc| doc._source}
+    search_param = params[:product]
+
+    unless search_param.nil?
+      query = Product.search(search_param)
+      response = query.response["hits"]["hits"]
+      @products = response.map { |doc| doc._source}
+    end
   end
 end
