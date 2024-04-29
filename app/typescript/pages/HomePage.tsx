e84397a943c376fd1a6ea8ba6fd5f275f2d4122c
Brian Wong <brianw7218@gmail.com>
2024-04-29 11:56:43 -0700

    Add state and typing for search results

--- app/typescript/pages/HomePage.tsx
+++ app/typescript/pages/HomePage.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import SearchForm from "../components/SearchForm/SearchForm";
+import ProductType from "../types/product.type";
 
 const HomePage = () => {
+  const [searchResults, setSearchResults] = useState<ProductType[]>([]);
+
   return (
     <>
       <h1>ElasticSearch - Search for Products</h1>
---
+++ app/typescript/types/product.type.ts
@@ -0,0 +1,11 @@
+type ProductType = {
+  created: Date;
+  description: string;
+  in_stock: number;
+  is_active: boolean;
+  name: string;
+  price: number;
+  sold: number;
+};
+
+export default ProductType;
