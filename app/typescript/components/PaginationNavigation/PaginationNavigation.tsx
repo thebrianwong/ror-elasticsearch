2d49c3d3852781af2dd9b9dd56e768c5c70fa766
Brian Wong <brianw7218@gmail.com>
2024-04-30 11:12:23 -0700

    Prevent navigating to the current page

--- app/typescript/components/PaginationNavigation/PaginationNavigation.tsx
+++ app/typescript/components/PaginationNavigation/PaginationNavigation.tsx
@@ -11,18 +11,22 @@ const PaginationNavigation = ({
       {Array(totalPages)
         .fill(undefined)
         .map((_, index) => {
-          return (
-            <a
-              href="/"
-              key={index}
-              onClick={(e) => {
-                e.preventDefault();
-                navigateToPage(searchValue, index + 1);
-              }}
-            >
-              {index + 1}
-            </a>
-          );
+          if (currentPage === index + 1) {
+            return <span key={index}>{index + 1}</span>;
+          } else {
+            return (
+              <a
+                href="/"
+                key={index}
+                onClick={(e) => {
+                  e.preventDefault();
+                  navigateToPage(searchValue, index + 1);
+                }}
+              >
+                {index + 1}
+              </a>
+            );
+          }
         })}
     </div>
   );
