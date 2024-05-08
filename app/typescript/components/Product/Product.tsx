e6b5016a2a7e869ba446010917f1106102d44446
Brian Wong <brianw7218@gmail.com>
2024-05-08 14:39:53 -0700

    Add grid gap spacing

--- app/typescript/components/Product/Product.tsx
+++ app/typescript/components/Product/Product.tsx
@@ -2,7 +2,7 @@ import ProductProps from "./type";
 
 const Product = ({ product }: ProductProps) => {
   return (
-    <div className="max-w-80 border-solid border-zinc-400 border-2 rounded-lg p-4 bg-amber-200 grid grid-cols-3">
+    <div className="max-w-80 border-solid border-zinc-400 border-2 rounded-lg p-4 bg-amber-200 grid grid-cols-3 gap-x-2">
       <div className="text-left divide-y divide-gray-900">
         <div className="my-2">
           <h2 className="text-lg font-semibold leading-none">{product.name}</h2>
