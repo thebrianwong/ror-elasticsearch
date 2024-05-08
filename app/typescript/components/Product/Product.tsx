18c9647fa8b896864a2416511b70bd87ab183aaa
Brian Wong <brianw7218@gmail.com>
2024-05-08 13:53:06 -0700

    Render more info about products

--- app/typescript/components/Product/Product.tsx
+++ app/typescript/components/Product/Product.tsx
@@ -3,7 +3,15 @@ import ProductProps from "./type";
 const Product = ({ product }: ProductProps) => {
   return (
     <li>
-      <div>{product.name}</div>
+      <div>
+        <h2 className="text-lg font-semibold">{product.name}</h2>
+        <p className="font-thin italic">{product.created.toString()}</p>
+        <p>{product.in_stock}</p>
+        <p>{product.is_active.toString()}</p>
+        <p>{product.price}</p>
+        <p>{product.sold}</p>
+        <p>{product.description}</p>
+      </div>
     </li>
   );
 };
