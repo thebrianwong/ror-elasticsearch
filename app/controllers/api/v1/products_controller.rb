bd955bbf36ed03df80ed01c00d9f1875945dc25c
Brian Wong <brianw7218@gmail.com>
2024-05-10 11:42:50 -0700

    Start to change the ElasticSearch query to include more search parameters

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,9 +1,7 @@
 class Api::V1::ProductsController < ApplicationController
   def index
-    search_param = params[:search_param]
-
-    unless search_param.nil?
-      query = Product.search(search_param).page(params[:page])
+    unless params[:search_value].nil?
+      query = Product.search(params).page(params[:page])
       response = query.response["hits"]["hits"]
       products = response.map { |doc| doc._source}
       num_of_pages = query.records.total_pages
--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -10,9 +10,26 @@ module Searchable
       indexes :title, type: :text
     end
 
-    def self.search(query)
+    def self.search(search_params)
       # build and run search
-      self.__elasticsearch__.search(query)
+      search_multi_match = {
+        multi_match: {
+          query: search_params[:search_value],
+          fields: ["name^2", :description, "tags^3"]
+        }
+      }
+
+      must_search_array = [search_multi_match]
+
+      query_params = {
+        query: {
+          bool: {
+            must: must_search_array
+          }
+        }
+      }
+
+      self.__elasticsearch__.search(query_params)
     end
   end
 end
