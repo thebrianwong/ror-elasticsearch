da91e08503402d6654aee61642df173e0a357141
Brian Wong <brianw7218@gmail.com>
2024-05-10 10:15:19 -0700

    Adjust button styling

--- app/typescript/components/BackToTopButton/BackToTopButton.tsx
+++ app/typescript/components/BackToTopButton/BackToTopButton.tsx
@@ -3,7 +3,7 @@ import BackToTopButtonProps from "./type";
 const BackToTopButton = ({ elementRef }: BackToTopButtonProps) => {
   return (
     <button
-      className="fixed bottom-16 right-16 bg-zinc-500 rounded-xl w-6 h-6"
+      className="fixed bottom-16 right-16 bg-zinc-500 rounded-3xl w-8 h-8 text-slate-50 font-bold"
       onClick={() =>
         elementRef?.current?.scrollIntoView({ behavior: "smooth" })
       }
