72ac44a6616730c7d17156a8e090f6d5d3690d10
Brian Wong <brianw7218@gmail.com>
2024-05-20 14:27:12 -0700

    Add the ability to query by sold in GraphQL

--- app/graphql/types/product_query_input.rb
+++ app/graphql/types/product_query_input.rb
@@ -10,5 +10,7 @@ module Types
     argument :in_stock_to, Integer, required: false, default_value: nil
     argument :price_from, Integer, required: false, default_value: nil
     argument :price_to, Integer, required: false, default_value: nil
+    argument :sold_from, Integer, required: false, default_value: nil
+    argument :sold_to, Integer, required: false, default_value: nil
   end
 end
